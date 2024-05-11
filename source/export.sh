353f76dc359cff7d339c4ccc64ef603c772ecd7b
grifinas <grifinas3@gmail.com>
2024-05-11 16:43:06 +0300

    simplify sturcutre

---
+++ source/export.sh
@@ -0,0 +1,19 @@
+if [[ -z "$RIMWORLD_MOD_PATH" ]]; then
+    RIMWORLD_MOD_PATH="../.export/"
+    mkdir "../.export"
+    echo "no RIMWORLD_MOD_PATH set exporing to ../.export"
+else
+    echo "exporing to $RIMWORLD_MOD_PATH"
+fi
+
+function recursiveCopyExt() {
+    for filepath in $(find . -type f -name "*.$1");
+    do
+        cp --parents -f "$filepath" $RIMWORLD_MOD_PATH
+    done
+}
+
+cp -rf "./About" "$RIMWORLD_MOD_PATH/About"
+recursiveCopyExt "xml"
+recursiveCopyExt "dll"
+recursiveCopyExt "png"
